db4f656b3b07c7fdd2077fe48bf4ed24e73cb26d
Jain Johnson <judejain@gmail.com>
2024-11-29 22:41:20 +0530

    role

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
 
-  f
+  firstName: string ="Angular Tutorial";
+  angularVersion="version 18";
+  version:number=18;
+  isActive:boolean=false;
+  currentDate: Date =new Date;
+  inputType:string="radio";
+
+  showWelcomeAlert()
+  {
+    alert("dsds")
+
+  }
+
+  showMessage(message:string)
+  {
+    alert(message)
+
+  }
 
 }
