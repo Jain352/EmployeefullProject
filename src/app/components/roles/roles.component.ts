e56b48097ff56f4789abe82413b30d2571e94f8e
Jain Johnson <judejain@gmail.com>
2024-11-29 22:40:56 +0530

    yes

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  imports: [],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+
+  f
+
+}
