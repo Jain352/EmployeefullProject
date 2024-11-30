5ebdc19ec43013ba48c9f2d3da8f6761694098d8
Jain Johnson <judejain@gmail.com>
2024-11-30 09:42:44 +0530

    GetAllRoles

--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { DesignationComponent } from '../designation/designation.component';
+import { RolesComponent } from '../roles/roles.component';
 
 @Component({
   selector: 'app-master',
-  imports: [],
+  imports: [DesignationComponent,RolesComponent],
   templateUrl: './master.component.html',
   styleUrl: './master.component.css'
 })
 export class MasterComponent {
+currentComponent:string="";
 
 }
