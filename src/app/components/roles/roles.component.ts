e086b2d59a65cf4dba940301b14edda7f8c9e82a
Jain Johnson <judejain@gmail.com>
2024-11-30 10:25:39 +0530

    list role ngfor

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,24 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component,inject,OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Interface } from 'readline';
+import { Irole } from '../../model/interface/role';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent implements OnInit{
 
-  roleList:Interface[]=[];
+  roleList:Irole[]=[];
 
   http=inject(HttpClient);
 
   ngOnInit(): void 
   {
     this.getAllRoles()
-    //alert("nter into ngin");
+    alert("nter into ngin");
   }
 
   getAllRoles()
@@ -30,7 +32,7 @@ this.http.get("https://freeapi.miniprojectideas.com/api/EmployeeApp/GetAllRoles"
 
 
 
-//alert("nter into ngtwon");
+alert("nter into ngtwon");
   }
 
 
