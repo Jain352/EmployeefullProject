4781db314d189a38002e8fa265cc58b731aecdc3
Jain Johnson <judejain@gmail.com>
2024-12-01 07:03:08 +0530

    client

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {Client} from '../../model/class/Client';import { FormsModule } from '@angular/forms';
-;
+import { ClientService } from '../../services/client.service';
+import { APIResponseModel } from '../../model/interface/role';
 
 @Component({
   selector: 'app-client',
@@ -8,9 +9,49 @@ import {Client} from '../../model/class/Client';import { FormsModule } from '@an
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit{
+
+  clientservice=inject(ClientService);
 
 clientObj:Client= new Client();
 clientList:Client[]=[];
+
+ngOnInit(): void {
+  this.loadClient();
+}
+
+loadClient()
+{
+  this.clientservice.getAllClients().subscribe((result:APIResponseModel)=>
+    {
+      this.clientList=result.data;
+     // this.isLoader=false;         
+    }
+  ,
+  error=>{alert("API Block")
+
+  })
+  
+}
+
+onSaveClient()
+{
+  debugger;
+  this.clientservice.addUpdate
+  (this.clientObj).subscribe((res:APIResponseModel)=>{
+    if(res.result)
+    {
+      alert("Client Created Sucess")
+      this.loadClient();
+    }
+    else{
+    alert(res.message)
+    }
+  })
+}
+deleteClientById()
+{
+  
+}
 
 }
--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -13,6 +13,8 @@ export class MasterService {
   getDesignation():Observable<APIResponseModel>
   {
     return this.http.
-    get<APIResponseModel>("https://freeapi.miniprojectideas.com/api/EmployeeApp/GetAllDesignation");
+    get<APIResponseModel>("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllDesignation");
   }
+ 
+
 }
